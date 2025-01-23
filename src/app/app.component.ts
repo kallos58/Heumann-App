907e68c7620ed7b137de7633836a5f6e2a90ed24
Bernd Maass <bernd.maass_ext@uid.com>
2025-01-23 18:42:11 +0100

    Ng 16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello 15 {{value}}</div>`,
+  template: `<div>Hello 16 {{value}}</div>`,
 })
 export class AppComponent {
   value = 'World';
