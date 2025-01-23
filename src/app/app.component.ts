c0196ec977d3a9102ebc9473a85021be9eb53dba
Bernd Maass <bernd.maass_ext@uid.com>
2025-01-23 21:03:39 +0100

    Kendo components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { KENDO_BUTTONS } from "@progress/kendo-angular-buttons";
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common'
 import { FormsModule } from '@angular/forms';
@@ -6,7 +5,7 @@ import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [KENDO_BUTTONS, CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,46 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppComponent } from "./app.component";
-import { ButtonsModule } from '@progress/kendo-angular-buttons';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from "@angular/router";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CommonModule, APP_BASE_HREF } from "@angular/common";
 
+import { LayoutModule } from "@progress/kendo-angular-layout";
+import { IndicatorsModule } from "@progress/kendo-angular-indicators";
+import { IconsModule, SVGIcon } from "@progress/kendo-angular-icons";
+import { NavigationModule } from "@progress/kendo-angular-navigation";
+import { ButtonsModule } from "@progress/kendo-angular-buttons";
+
+export const bottomNavigationRoutes = [
+  {
+    path: "",
+    
+    selected: true,
+  },
+  {
+    path: "appbar",
+    
+    selected: false,
+  },
+  {
+    path: "actionsheet",
+    
+    selected: false,
+  },
+];
 
 
 @NgModule({
   imports: [
+    RouterModule.forRoot(bottomNavigationRoutes),
     BrowserModule,
     BrowserAnimationsModule,
-    ButtonsModule
+    CommonModule,
+    LayoutModule,
+    IndicatorsModule,
+    IconsModule,
+    NavigationModule,
+    ButtonsModule,
   ],
   bootstrap: [AppComponent],
   providers: [
