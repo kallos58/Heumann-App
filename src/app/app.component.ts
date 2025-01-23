6e33712a392641af4dbf2997ce1bc5d8576f93b2
Bernd Maass <bernd.maass_ext@uid.com>
2025-01-23 20:15:35 +0100

    Kendo buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,30 @@
-import { Component } from '@angular/core';
+import { KENDO_BUTTONS } from "@progress/kendo-angular-buttons";
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello 18 {{value}}</div>`,
+  standalone: true,
+  imports: [KENDO_BUTTONS, CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  value = 'World';
+
+  entries = 0;
+  constructor() {
+   
+  }
+
+
+  cardClick(i: number) {
+
+  }
+
+  exportToXls() {
+
+  }
+
+  clearFilter() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { AppComponent } from "./app.component";
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  bootstrap: [AppComponent]
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonsModule
+  ],
+  bootstrap: [AppComponent],
+  providers: [
+    provideAnimationsAsync()
+  ]
 })
 export class AppModule {}
+
